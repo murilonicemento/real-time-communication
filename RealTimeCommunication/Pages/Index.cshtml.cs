1db6830b67458771af84fae86cabb614416fadf9
Murilo Nascimento <murilo_nascimento22@outlook.com>
2026-03-08 19:08:49 -0300

    initial commit

---
+++ RealTimeCommunication/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealTimeCommunication.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
